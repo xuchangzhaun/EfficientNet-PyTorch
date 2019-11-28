ca695cf3d7186d54c5e2bb368c7f582356f31bc8
jcoral <1355539304@qq.com>
2019-11-28 22:21:26 +0800

    Update model.py

--- efficientnet_pytorch/model.py
+++ efficientnet_pytorch/model.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from efficient_net.efficientnet_pytorch.utils import (
+from .utils import (
     relu_fn,
     hswish,
     hsigmoid,
